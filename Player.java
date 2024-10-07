52b090973e742f5b40dc4cb3a23612df7c6a8849
Sus-Pancake <89539674+Sus-Pancake@users.noreply.github.com>
2024-10-07 14:43:32 +0000

    proggers

--- Player.java
+++ Player.java
@@ -1,4 +1,17 @@
 public class Player {
     //static variables
     public static int health = 100;
+
+
+    public static void heal(int currentLevel) {
+
+        int heal = (int)(Math.random() * 5) + 1;
+
+        heal -= (int)(currentLevel * .5);
+
+        if(heal > 0) health += heal;
+        else heal = 0;
+
+        System.out.println("You healed for " + heal + " points");
+    }
 }
